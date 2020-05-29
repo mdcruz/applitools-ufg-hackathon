8828add95896fd92bdb6bf9cdd0f530b554d7032
mdcruz <marie.cruz1@news.co.uk>
2020-05-29 21:47:05 +0100

    add traditional test for task 2

--- TraditionalTestsV1/cypress/plugins/index.js
+++ TraditionalTestsV1/cypress/plugins/index.js
@@ -1,13 +1,1 @@
-const fs = require('fs-extra');
-const path = require('path');
-
-const getConfigurationByFile = (file) => {
-  const pathToConfigFile = `../../../cypress.${file}.json`;
-  return !!file && fs.readJson(path.join(__dirname, '../', pathToConfigFile));
-};
-
-module.exports = (on, config) => {
-  const enviroment = config.env.configFile;
-  const configForEnviroment = getConfigurationByFile(enviroment);
-  return configForEnviroment || config;
-};
+module.exports = (on, config) => {};
